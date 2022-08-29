e80dab30a69811fabb49eedcb0d4e01b68d2d73e
Darkherobire <111693997+Darkherobire@users.noreply.github.com>
2022-08-29 07:20:33 +0700

    CameraCoding

---
+++ ProjectMoveOn/Assets/Scripts/Camera/CameraSwitcher.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public static class CameraSwitcher
+{
+    private static List<CinemachineVirtualCamera> cameras = new List<CinemachineVirtualCamera>();
+
+    public static CinemachineVirtualCamera ActiveCamera = null;
+
+    public static bool IsActiveCamera(CinemachineVirtualCamera camera)
+    {
+        return camera == ActiveCamera;
+    }
+    
+    public static void SwitchCamera(CinemachineVirtualCamera camera)
+    {
+        camera.Priority = 10;
+        ActiveCamera = camera;
+
+        foreach (CinemachineVirtualCamera c in cameras)
+        {
+            if (c != camera && c.Priority != 0)
+            {
+                c.Priority = 0;
+            }
+        }
+    }
+
+    public static void Register(CinemachineVirtualCamera camera)
+    {
+        cameras.Add(camera);
+        Debug.Log("Camera registerd: " + camera);
+    }
+
+    public static void Unregister(CinemachineVirtualCamera camera)
+    {
+        cameras.Remove(camera);
+        Debug.Log("Camera unregisterd: " + camera);
+    }
+    
+}
