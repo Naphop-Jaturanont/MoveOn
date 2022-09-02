e4fa67d611edf1d6d7123da3a6e2f70a381c9ab6
Darkherobire <111693997+Darkherobire@users.noreply.github.com>
2022-09-02 22:10:53 +0700

    update

--- ProjectMoveOn/Assets/Scripts/CheckPoint.cs
+++ ProjectMoveOn/Assets/Scripts/CheckPoint.cs
@@ -7,20 +7,27 @@ public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private List<GameObject> checkPoint;
-    [SerializeField] private Vector3 vertorPoint;
-    [SerializeField] private int dead;// int or float
+
+    private Vector3 _spawnPoint;
+    private void Start()
+    {
+        _spawnPoint = gameObject.transform.position;
+    }
 
     private void Update()
     {
-        if (player.transform.position.y < -dead)
+        if (player.transform.position.y < -20f)
         {
-            player.transform.position = vertorPoint;
+            player.transform.position = _spawnPoint;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        vertorPoint = player.transform.position;
-        Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("CheckPoint"))
+        {
+            _spawnPoint = gameObject.transform.position;
+            Destroy(other.gameObject);
+        }
     }
 }
