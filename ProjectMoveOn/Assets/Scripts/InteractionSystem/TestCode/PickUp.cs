3798c28e687fd6a4847d22a5045826d0643ead4c
pichada00 <pichada.kon@gmail.com>
2022-09-09 22:11:22 +0700

    Revert "Revert "Update""
    This reverts commit 833c919c82c02493b0e5efb99fa12b231003f1fa.

---
+++ ProjectMoveOn/Assets/Scripts/InteractionSystem/TestCode/PickUp.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PickUp : MonoBehaviour
+{
+
+    [SerializeField] private Transform PickUpPointR;
+    [SerializeField] private Transform PickUpPointL;
+
+    private Transform player;
+
+    public bool itemIsPickedR = false;
+    public bool itemIsPickedL = false;
+    
+    private Rigidbody rb;
+    
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("PlayerArmature").transform;
+        PickUpPointR = GameObject.Find("PickupPointR").transform;
+        PickUpPointL = GameObject.Find("PickupPointL").transform;
+    }
+    void Update()
+    {
+        DropR();
+        DropL();
+    }
+    void OnTriggerStay(Collider player)
+    {
+        if(player.tag == "Player")
+        {
+            if(itemIsPickedR == false)
+            {
+                if (Keyboard.current.eKey.wasPressedThisFrame && PickUpPointR.childCount < 1)
+                {
+                    itemIsPickedR = true;
+                    GetComponent<Rigidbody>().useGravity = false;
+                    GetComponent<Collider>().enabled = false;
+                    GetComponent<SphereCollider>().enabled = false;
+                    this.transform.position = PickUpPointR.position;
+                    this.transform.parent = GameObject.Find("PickupPointR").transform;
+                }
+            }
+
+            if (itemIsPickedL == false)
+            {
+                if(Keyboard.current.qKey.wasPressedThisFrame && PickUpPointL.childCount < 1)
+                {
+                    itemIsPickedL = true;
+                    GetComponent<Rigidbody>().useGravity = false;
+                    GetComponent<Collider>().enabled = false;
+                    GetComponent<SphereCollider>().enabled = false;
+                    this.transform.position = PickUpPointL.position;
+                    this.transform.parent = GameObject.Find("PickupPointL").transform;
+                    
+                }
+            }
+        }
+    }
+    private void DropR()
+    {
+        if (itemIsPickedR == true)
+        {
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                itemIsPickedR = false;
+                this.transform.parent = null;
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<Collider>().enabled = true;
+                GetComponent<SphereCollider>().enabled = true;
+            }
+        }
+    }
+    private void DropL()
+    {
+        if (itemIsPickedL == true)
+        {
+            if (Keyboard.current.qKey.wasPressedThisFrame)
+            {
+                itemIsPickedL = false;
+                this.transform.parent = null;
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<Collider>().enabled = true;
+                GetComponent<SphereCollider>().enabled = true;
+            }
+        }
+    }
+}
