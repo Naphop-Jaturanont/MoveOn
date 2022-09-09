c47f7d61713885f4be2efcaf554cafbf06e6836e
0rphnoch <numchai.auts@gmail.com>
2022-09-09 20:47:30 +0700

    UpdateScene

--- ProjectMoveOn/Assets/Scripts/InteractionSystem/TestCode/PickUpItem.cs
+++ ProjectMoveOn/Assets/Scripts/InteractionSystem/TestCode/PickUpItem.cs
@@ -12,6 +12,8 @@ public class PickUpItem : MonoBehaviour
 
     public bool itemIsPicked;
 
+    public bool itemInteracted = false;
+
     private Rigidbody rb;
     void Start()
     {
@@ -26,16 +28,70 @@ public class PickUpItem : MonoBehaviour
 
         if(pickUpDistance <= 2)
         {
+<<<<<<< Updated upstream
             if (Keyboard.current.eKey.wasPressedThisFrame && itemIsPicked == false && PickUpPoint.childCount < 1)
             {
                 GetComponent<Rigidbody>().useGravity = false;
                 GetComponent<BoxCollider>().enabled = false;
                 this.transform.position = PickUpPoint.position;
                 this.transform.parent = GameObject.Find("PickupPoint").transform;
+=======
+            if (Keyboard.current.eKey.wasPressedThisFrame )
+            {
+                
+                if (itemIsPickedR ==false && PickUpPointR.childCount < 1 && itemInteracted == false )
+                {
+                    GetComponent<Rigidbody>().useGravity = false;
+                    GetComponent<Collider>().isTrigger = true;
+                    this.transform.position = PickUpPointR.position;
+                    this.transform.parent = GameObject.Find("PickupPointR").transform;
+                    itemIsPickedR = true;
+                    Invoke("ChangeBool", 0.75f);
+                    Debug.Log("R:"+itemIsPickedR.ToString());
+                    if(this.gameObject.name == "Lamp (2)")
+                    {
+                        StarterAssets.ThirdPersonController.Instance.keepLamb = true;
+                    }
+                }
+                else
+                {
+                    this.transform.parent = null;
+                    GetComponent<Rigidbody>().useGravity = true;
+                    GetComponent<Collider>().isTrigger = false;
+                    itemIsPickedR = false;
+                    itemInteracted = false;
+                    Debug.Log("R"+itemIsPickedR.ToString());
+                }
+                
+            }
+
+            if (Keyboard.current.qKey.wasPressedThisFrame)
+            {
+                if (itemIsPickedL == false && PickUpPointL.childCount < 1 && itemInteracted == false)
+                {
+                    GetComponent<Rigidbody>().useGravity = false;
+                    GetComponent<Collider>().isTrigger = true;
+                    this.transform.position = PickUpPointL.position;
+                    this.transform.parent = GameObject.Find("PickupPointL").transform;
+                    itemIsPickedL = true;
+                    Invoke("ChangeBool", 0.75f);
+                    Debug.Log("L"+itemIsPickedL.ToString());
+                }
+                else
+                {
+                    this.transform.parent = null;
+                    GetComponent<Rigidbody>().useGravity = true;
+                    GetComponent<Collider>().isTrigger = false;
+                    itemIsPickedL = false;
+                    itemInteracted = false;
+                    Debug.Log("L" + itemIsPickedL.ToString());
+                }
+>>>>>>> Stashed changes
 
                 itemIsPicked = true;
             }
         }
+<<<<<<< Updated upstream
 
         if(Keyboard.current.qKey.wasPressedThisFrame&& itemIsPicked == true)
         {
@@ -45,4 +101,14 @@ public class PickUpItem : MonoBehaviour
             itemIsPicked = false;
         }
     }
+=======
+        
+    }
+    public void ChangeBool()
+    {
+        itemInteracted = true;
+    }
+    
+
+>>>>>>> Stashed changes
 }
